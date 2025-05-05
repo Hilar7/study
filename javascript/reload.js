a949699c5016959fab234b6aeac6039ce4df2895
Hilar7 <danildorsh7@mail.ru>
2025-05-05 12:35:18 +0700

    Финиш

--- javascript/reload.js
+++ javascript/reload.js
@@ -9,22 +9,3 @@ window.addEventListener('load', function() {
         }, 500);
     }, Math.max(0, minDisplayTime - loadTime));
 });
-
-let startX = 0;
-const commentCarousel = document.querySelector('#carouselExample');
-
-commentCarousel.addEventListener('touchstart', (e) => {
-    startX = e.touches[0].clientX;
-});
-
-commentCarousel.addEventListener('touchend', (e) => {
-    const endX = e.changedTouches[0].clientX;
-    if (startX - endX > 50) {
-        // Свайп влево - следующий слайд
-        $('.carousel').carousel('next');
-    }
-    if (endX - startX > 50) {
-        // Свайп вправо - предыдущий слайд
-        $('.carousel').carousel('prev');
-    }
-});
